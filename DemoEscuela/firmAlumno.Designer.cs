2e4994558416d613236ac81c3d960ab1d70e90b2
16171469 <16171469@itculiacan.edu.mx>
2020-12-02 10:37:56 -0700

    DEMO ESCUELA

---
+++ DemoEscuela/firmAlumno.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace DemoEscuela
+{
+    partial class firmAlumno
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idAlumno = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.ApellidoPaterno = new System.Windows.Forms.Label();
+            this.ApellidoMaterno = new System.Windows.Forms.Label();
+            this.textBoxIdAlumno = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxApellidoPaterno = new System.Windows.Forms.TextBox();
+            this.textBoxApellidoMaterno = new System.Windows.Forms.TextBox();
+            this.pictureBoxFotografia = new System.Windows.Forms.PictureBox();
+            this.Examinar = new System.Windows.Forms.Button();
+            this.textBoxRutaImagen = new System.Windows.Forms.TextBox();
+            this.Fotografia = new System.Windows.Forms.Label();
+            this.buttonAgregar = new System.Windows.Forms.Button();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.buttonActualizar = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFotografia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // idAlumno
+            // 
+            this.idAlumno.AutoSize = true;
+            this.idAlumno.Location = new System.Drawing.Point(45, 30);
+            this.idAlumno.Name = "idAlumno";
+            this.idAlumno.Size = new System.Drawing.Size(53, 13);
+            this.idAlumno.TabIndex = 0;
+            this.idAlumno.Text = "idAlumno:";
+            this.idAlumno.Click += new System.EventHandler(this.idAlumno_Click);
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(45, 70);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(47, 13);
+            this.Nombre.TabIndex = 0;
+            this.Nombre.Text = "Nombre:";
+            this.Nombre.Click += new System.EventHandler(this.Nombre_Click);
+            // 
+            // ApellidoPaterno
+            // 
+            this.ApellidoPaterno.AutoSize = true;
+            this.ApellidoPaterno.Location = new System.Drawing.Point(11, 106);
+            this.ApellidoPaterno.Name = "ApellidoPaterno";
+            this.ApellidoPaterno.Size = new System.Drawing.Size(87, 13);
+            this.ApellidoPaterno.TabIndex = 0;
+            this.ApellidoPaterno.Text = "Apellido Paterno:";
+            this.ApellidoPaterno.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // ApellidoMaterno
+            // 
+            this.ApellidoMaterno.AutoSize = true;
+            this.ApellidoMaterno.Location = new System.Drawing.Point(3, 147);
+            this.ApellidoMaterno.Name = "ApellidoMaterno";
+            this.ApellidoMaterno.Size = new System.Drawing.Size(89, 13);
+            this.ApellidoMaterno.TabIndex = 0;
+            this.ApellidoMaterno.Text = "Apellido Materno:";
+            this.ApellidoMaterno.Click += new System.EventHandler(this.ApellidoMaterno_Click);
+            // 
+            // textBoxIdAlumno
+            // 
+            this.textBoxIdAlumno.Location = new System.Drawing.Point(104, 30);
+            this.textBoxIdAlumno.Name = "textBoxIdAlumno";
+            this.textBoxIdAlumno.Size = new System.Drawing.Size(148, 20);
+            this.textBoxIdAlumno.TabIndex = 1;
+            this.textBoxIdAlumno.TextChanged += new System.EventHandler(this.textBoxIdAlumno_TextChanged);
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(104, 70);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(148, 20);
+            this.textBoxNombre.TabIndex = 1;
+            this.textBoxNombre.TextChanged += new System.EventHandler(this.textBoxNombre_TextChanged);
+            // 
+            // textBoxApellidoPaterno
+            // 
+            this.textBoxApellidoPaterno.Location = new System.Drawing.Point(104, 106);
+            this.textBoxApellidoPaterno.Name = "textBoxApellidoPaterno";
+            this.textBoxApellidoPaterno.Size = new System.Drawing.Size(148, 20);
+            this.textBoxApellidoPaterno.TabIndex = 1;
+            this.textBoxApellidoPaterno.TextChanged += new System.EventHandler(this.textBoxApellidoPaterno_TextChanged);
+            // 
+            // textBoxApellidoMaterno
+            // 
+            this.textBoxApellidoMaterno.Location = new System.Drawing.Point(104, 147);
+            this.textBoxApellidoMaterno.Name = "textBoxApellidoMaterno";
+            this.textBoxApellidoMaterno.Size = new System.Drawing.Size(148, 20);
+            this.textBoxApellidoMaterno.TabIndex = 1;
+            this.textBoxApellidoMaterno.TextChanged += new System.EventHandler(this.textBoxApellidoMaterno_TextChanged);
+            // 
+            // pictureBoxFotografia
+            // 
+            this.pictureBoxFotografia.Location = new System.Drawing.Point(275, 32);
+            this.pictureBoxFotografia.Name = "pictureBoxFotografia";
+            this.pictureBoxFotografia.Size = new System.Drawing.Size(194, 134);
+            this.pictureBoxFotografia.TabIndex = 2;
+            this.pictureBoxFotografia.TabStop = false;
+            // 
+            // Examinar
+            // 
+            this.Examinar.Location = new System.Drawing.Point(273, 183);
+            this.Examinar.Name = "Examinar";
+            this.Examinar.Size = new System.Drawing.Size(194, 20);
+            this.Examinar.TabIndex = 4;
+            this.Examinar.Text = "Examinar";
+            this.Examinar.UseVisualStyleBackColor = true;
+            this.Examinar.Click += new System.EventHandler(this.Examinar_Click);
+            // 
+            // textBoxRutaImagen
+            // 
+            this.textBoxRutaImagen.Location = new System.Drawing.Point(104, 183);
+            this.textBoxRutaImagen.Name = "textBoxRutaImagen";
+            this.textBoxRutaImagen.Size = new System.Drawing.Size(148, 20);
+            this.textBoxRutaImagen.TabIndex = 3;
+            // 
+            // Fotografia
+            // 
+            this.Fotografia.AutoSize = true;
+            this.Fotografia.Location = new System.Drawing.Point(33, 186);
+            this.Fotografia.Name = "Fotografia";
+            this.Fotografia.Size = new System.Drawing.Size(59, 13);
+            this.Fotografia.TabIndex = 5;
+            this.Fotografia.Text = "Fotografía:";
+            // 
+            // buttonAgregar
+            // 
+            this.buttonAgregar.Location = new System.Drawing.Point(39, 234);
+            this.buttonAgregar.Name = "buttonAgregar";
+            this.buttonAgregar.Size = new System.Drawing.Size(74, 23);
+            this.buttonAgregar.TabIndex = 6;
+            this.buttonAgregar.Text = "Agregar";
+            this.buttonAgregar.UseVisualStyleBackColor = true;
+            this.buttonAgregar.Click += new System.EventHandler(this.buttonAgregar_Click);
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.Location = new System.Drawing.Point(120, 234);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(74, 23);
+            this.buttonEliminar.TabIndex = 6;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = true;
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(275, 3);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(192, 23);
+            this.buttonBuscar.TabIndex = 6;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // buttonActualizar
+            // 
+            this.buttonActualizar.Location = new System.Drawing.Point(209, 234);
+            this.buttonActualizar.Name = "buttonActualizar";
+            this.buttonActualizar.Size = new System.Drawing.Size(74, 23);
+            this.buttonActualizar.TabIndex = 6;
+            this.buttonActualizar.Text = "Actualizar";
+            this.buttonActualizar.UseVisualStyleBackColor = true;
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(5, 286);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(493, 164);
+            this.dgvDatos.TabIndex = 7;
+            // 
+            // firmAlumno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 462);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.buttonActualizar);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.buttonAgregar);
+            this.Controls.Add(this.Fotografia);
+            this.Controls.Add(this.Examinar);
+            this.Controls.Add(this.textBoxRutaImagen);
+            this.Controls.Add(this.pictureBoxFotografia);
+            this.Controls.Add(this.textBoxApellidoMaterno);
+            this.Controls.Add(this.textBoxApellidoPaterno);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textBoxIdAlumno);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.ApellidoMaterno);
+            this.Controls.Add(this.ApellidoPaterno);
+            this.Controls.Add(this.idAlumno);
+            this.Name = "firmAlumno";
+            this.Text = "Alumno";
+            this.Load += new System.EventHandler(this.firmAlumno_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFotografia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label idAlumno;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.Label ApellidoPaterno;
+        private System.Windows.Forms.Label ApellidoMaterno;
+        private System.Windows.Forms.TextBox textBoxIdAlumno;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxApellidoPaterno;
+        private System.Windows.Forms.TextBox textBoxApellidoMaterno;
+        private System.Windows.Forms.PictureBox pictureBoxFotografia;
+        private System.Windows.Forms.Button Examinar;
+        private System.Windows.Forms.TextBox textBoxRutaImagen;
+        private System.Windows.Forms.Label Fotografia;
+        private System.Windows.Forms.Button buttonAgregar;
+        private System.Windows.Forms.Button buttonEliminar;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.Button buttonActualizar;
+        private System.Windows.Forms.DataGridView dgvDatos;
+    }
+}
+
---
+++ DemoEscuela/firmAlumno.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DemoEscuela
+{
+    public partial class firmAlumno : Form
+    {
+        EscuelaDBEntities contexto;
+
+        public firmAlumno()
+        {
+            InitializeComponent();
+        }
+
+        private void firmAlumno_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxApellidoMaterno_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxApellidoPaterno_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxIdAlumno_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void idAlumno_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ApellidoMaterno_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LimpiarTextos()
+        {
+            textBoxIdAlumno.Text = "";
+            textBoxNombre.Text = "";
+            textBoxApellidoPaterno.Text = "";
+            textBoxApellidoMaterno.Text = "";
+            textBoxRutaImagen.Text = "";
+            pictureBoxFotografia.ImageLocation = "";
+        }
+
+        private void LlenarGrid()
+        {
+            try
+            {
+                contexto = new EscuelaDBEntities();
+                var datos = from a in contexto.Alumnoes
+                            select new
+                            {
+                                Nombre = a.Nombre,
+                                ApellidoPaterno = a.ApellidoPaterno,
+                                ApellidoMaterno = a.ApellidoMaterno                      
+                            };
+                dgvDatos.DataSource = datos.ToList();
+                dgvDatos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Ocurrio un error: " + ex.ToString());
+            }
+        }
+
+        private void buttonAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                contexto = new EscuelaDBEntities();
+
+                //Creamos un nuevo alumno
+                Alumno alumno = new Alumno()
+                {
+                    Id = int.Parse(textBoxIdAlumno.Text),
+                    Nombre = textBoxNombre.Text,
+                    ApellidoPaterno = textBoxApellidoPaterno.Text,
+                    ApellidoMaterno = textBoxApellidoMaterno.Text,
+                    Fotografía = textBoxRutaImagen.Text,
+                };
+
+                //Agregar el objeto a la BD
+                contexto.Alumnoes.Add(alumno);
+                contexto.SaveChanges();
+                MessageBox.Show("Alumno agregado correctamente");
+                LimpiarTextos();
+                LlenarGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrio un error: " + ex.ToString());
+
+            }
+
+        }
+
+        private void Examinar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openfile = new OpenFileDialog();
+                openfile.Title = "Abrir Imagen";
+                openfile.Filter = "Archivos.JPG (*.jpg, *.jpeg)| *.jpg; *.jpeg ";
+
+                if (openfile.ShowDialog() == DialogResult.OK)
+                {
+                    string image = openfile.FileName;
+                    textBoxRutaImagen.Text = image;
+                    pictureBoxFotografia.Image = Image.FromFile(image);
+                }
+                else
+                {
+                    MessageBox.Show("No se seleccioo Imagen");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrio un error: " + ex.ToString());
+            }
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBoxIdAlumno.Text != string.Empty)
+                {
+                    int clave = int.Parse(textBoxIdAlumno.Text);
+                    contexto = new EscuelaDBEntities();
+
+                    Alumno buscar = (from a in contexto.Alumnoes
+                                     where a.Id == clave
+                                     select a).SingleOrDefault();
+                    if (buscar!=null)
+                    {
+                        textBoxNombre.Text = buscar.Nombre;
+                        textBoxApellidoPaterno.Text = buscar.ApellidoPaterno;
+                        textBoxApellidoMaterno.Text = buscar.ApellidoMaterno;
+                        pictureBoxFotografia.Image = Image.FromFile(buscar.Fotografía);
+                    }
+                    else
+                    {
+                        LimpiarTextos();
+                        MessageBox.Show("No se encontro el registro");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Ocurrio un error: " + ex.ToString());
+            }
+        }
+    }
+}
